66490eda14571d955c1d8e09be951553fa796038
Iago Roque <71741741+iagoroque@users.noreply.github.com>
2022-06-13 09:23:20 -0300

    Adição do quickSort().

--- Estrutura de Dados/src/br/ucsal/ed/ordenacao/Sort.java
+++ Estrutura de Dados/src/br/ucsal/ed/ordenacao/Sort.java
@@ -1,13 +1,40 @@
 package br.ucsal.ed.ordenacao;
 
 public class Sort {
-	static int[] x = {7234, 323, 34345756, 9, 287};
+	static int[] x = {7234, 323, 343457, 9, 287};
 	int j, eleito, menor, pos, aux;
 	
 	public static void main(String[] args) {
 		Sort ordem = new Sort();
-		ordem.bubbleSort(x);
+		ordem.quickSort(x, 0, 4);
+		for (int i = 0; i < x.length; i++) {
+			System.out.print(x[i] + " ");
+		}
 	}
+	
+//	public static void main(String[] args) {
+//		int quantidade = 10;
+//		int[] vetor = new int[quantidade];
+//
+//		for (int i = 0; i < vetor.length; i++) {
+//			vetor[i] = (int) (Math.random() * quantidade);
+//		}
+//		
+//		for (int j = 0; j < vetor.length; j++) {
+//			System.out.print(vetor[j] + " ");
+//		}
+//
+//		long tempoInicial = System.currentTimeMillis();
+//
+//		quickSort(vetor, 0, 9);
+//
+//		long tempoFinal = System.currentTimeMillis();
+//
+//		System.out.println("Executado em = " + (tempoFinal - tempoInicial) + " ms");
+//		for (int j = 0; j < vetor.length; j++) {
+//			System.out.print(vetor[j] + " ");
+//		}
+//	}
 	
 	public void insertionSort(int[] x) {
 		for (int i = 0; i < x.length; i++) {
@@ -69,4 +96,71 @@ public class Sort {
 			// falta o intercala.
 		}
 	}
+	
+	public void quickSort(int[] x, int p, int r) {
+		int q;
+		if (p < r) {
+			q = particao(x, p, r);
+			quickSort(x, p, q);
+			quickSort(x, q + 1, r);
+		}
+	}
+
+	private int particao(int[] x, int p, int r) {
+		int pivo, i, j;
+		pivo = x[(p + r) / 2];
+		i = p - 1;
+		j = r + 1;
+		while (i < j) {
+			do {
+				j = j - 1;
+			} while (x[j] > pivo);
+			do {
+				i = i + 1;
+			} while (x[i] < pivo);
+			if (i < j) {
+				troca(x, i, j);
+			}
+		}
+		return j;
+	}
+
+	private void troca(int[] x, int i, int j) {
+		int aux;
+		aux = x[i];
+		x[i] = x[j];
+		x[j] = aux;
+	}
+	
+	// Metodo que eu peguei do TED de ED
+	public void quickSort2(int[] vetor, int inicio, int fim) {
+		if (inicio < fim) {
+			int posicaoPivo = separar(vetor, inicio, fim);
+			quickSort2(vetor, inicio, posicaoPivo - 1);
+			quickSort2(vetor, posicaoPivo + 1, fim);
+		}
+	}
+
+	private int separar(int[] vetor, int inicio, int fim) {
+		int pivo = vetor[inicio];
+		int i = inicio + 1, f = fim;
+		while (i <= f) {
+			if (vetor[i] <= pivo)
+				i++;
+			else if (pivo < vetor[f])
+				f--;
+			else {
+				int troca = vetor[i];
+				vetor[i] = vetor[f];
+				vetor[f] = troca;
+				i++;
+				f--;
+			}
+		}
+		vetor[inicio] = vetor[f];
+		vetor[f] = pivo;
+		return f;
+	}
+	
+	
 }
